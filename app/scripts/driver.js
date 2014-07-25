137c476036b3505097ae85c423ba61c74a1d4dfd
reedspool <splinterspool@yahoo.com>
2014-07-24 23:51:34 -0400

    Rev 0.0.0.0.0.1

--- app/scripts/driver.js
+++ app/scripts/driver.js
@@ -1,6 +1,193 @@
 /* Driver.js: Run our web app! */
 define(['d3', 'jquery', 'lodash', 'jquery.eventAggregator', 'knockout'],
   function(d3, $, _, eventAggregator, ko) {
-    // Start here!
+   	
+   	setInterval(Welcome_To_The_World_Of_Tomorrow, 0)
+
+
+   	function My_First_Console_Log(value) {
+   		// Congratulations if you stepped in here! This class probably isn't for you.
+
+   		// But stick around, and tell your friends!
+
+
+		console.log('YAAAAY WELCOME TO THE CONSOLE! THIS IS WHERE WE CAN BEGIN THE ACTION!!!')
+		
+		console.log('But first the answer to your query!!!!! HERE IT IS:')
+
+   		console.log(value);
+		
+		console.log('YAAAAAAAY')
+
+		console.log('THATS ALL FOR NOW, FOLKS!')
+   	}
+
+
+
+
+  	function Welcome_To_The_World_Of_Tomorrow() {
+  		/* Welcome, welcome welcome! */
+  		debugger;
+  		/* 
+  			One and all, to the big show!
+
+  			Today you learn how to code.
+
+  			Click "Continue". It's the Play Button. Find it now.
+  		*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  		/* Nice, you made it to my secret spot! */
+  		debugger;
+  		/*
+
+  			There's not much here right now, but I got big plans I tell ya!
+
+  			I'm gonna put a couch over there, and a sunroof over here
+
+  			I got this sweet painting, click "Continue" to see it....
+
+  			If you want....
+  		*/
+
+
+
+
+
+
+
+
+
+
+
+
+/* Scroll down to see the whole thing */
+
+// Then press "Continue"
+debugger;
+/*
+
+
+
+
+
+                        .s$$$Ss.
+            .8,         $$$. _. .              ..sS$$$$$"  ...,.;
+ o.   ,@..  88        =.$"$'  '          ..sS$$$$$$$$$$$$s. _;"'
+  @@@.@@@. .88.   `  ` ""l. .sS$$.._.sS$$$$$$$$$$$$S'"'
+   .@@@q@@.8888o.         .s$$$$$$$$$$$$$$$$$$$$$'
+     .:`@@@@33333.       .>$$$$$$$$$$$$$$$$$$$$'
+     .: `@@@@333'       ..>$$$$$$$$$$$$$$$$$$$'
+      :  `@@333.     `.,   s$$$$$$$$$$$$$$$$$'
+      :   `@33       $$ S.s$$$$$$$$$$$$$$$$$'
+      .S   `Y      ..`  ,"$' `$$$$$$$$$$$$$$
+      $s  .       ..S$s,    . .`$$$$$$$$$$$$.
+      $s .,      ,s ,$$$$,,sS$s.$$$$$$$$$$$$$.
+      / /$$SsS.s. ..s$$$$$$$$$$$$$$$$$$$$$$$$$.
+     /`.`$$$$$dN.ssS$$'`$$$$$$$$$$$$$$$$$$$$$$$.
+    ///   `$$$$$$$$$'    `$$$$$$$$$$$$$$$$$$$$$$.
+   ///|     `S$$S$'       `$$$$$$$$$$$$$$$$$$$$$$.
+  / /                      $$$$$$$$$$$$$$$$$$$$$.
+                           `$$$$$$$$$$$$$$$$$$$$$s.
+                            $$$"'        .?T$$$$$$$
+                           .$'        ...      ?$$#\
+                           !       -=S$$$$$s
+                         .!       -=s$$'  `$=-_      :
+                        ,        .$$$'     `$,       .|
+                       ,       .$$$'          .        ,
+                      ,     ..$$$'
+                          .s$$$'                 `s     .
+                   .   .s$$$$'                    $s. ..$s
+                  .  .s$$$$'                      `$s=s$$$
+                    .$$$$'                         ,    $$s
+               `   " .$$'                               $$$
+               ,   s$$'                              .  $$$s
+            ` .s..s$'                                .s ,$$
+             .s$$$'                                   "s$$$,
+          -   $$$'                                     .$$$$.
+        ."  .s$$s                                     .$',',$.
+        $s.s$$$$S..............   ................    $$....s$s......
+         `""'           `     ```"""""""""""""""         `""   ``
+                                                           [banksy]dp 
+
+		Isn't that totally rad? Where should I put it? Next to the couch?
+      */                                                     
+      debugger;
+      // Click "Continue"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      		// Okay let's get real
+      		debugger;
+      		// Press "Step Over"
+      		// It's the button that looks like a rainbow
+
+
+
+      		// Hey look, you're paused on a line other than
+      		// "debugger;" !!!!!!!!!!
+      		//
+      		// This is important. 
+      		//
+      		// How does that make you feel?
+      		// You will be able to tell me soon, young padawan.
+
+      		My_First_Console_Log(3*3*3*3*3*3*3*3*3*3*3*3*3*3*3)
+      		// Try pressing "Continue" now.
+
+
+
+
+
+			// You just ran some code! You did it! youyouyouyou
+			// But what's the answer?
+			// Open the "Console" to find out. It's a button that says "Console"
+      		debugger;
+
+
+
+
+
+
+
+
+  	}
+
+
   }
 );
