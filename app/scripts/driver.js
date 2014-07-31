4b9a072ed7f2947181b4c4f317d7837c7ff4019a
reedspool <splinterspool@yahoo.com>
2014-07-31 08:54:00 -0400

    First legit pass at chapter one

--- app/scripts/driver.js
+++ app/scripts/driver.js
@@ -1,76 +1,52 @@
-/* Driver.js: Run our web app! */
-define(['d3', 'jquery', 'lodash', 'jquery.eventAggregator', 'knockout'],
-  function(d3, $, _, eventAggregator, ko) {
-   	
-   	setInterval(Welcome_To_The_World_Of_Tomorrow, 0)
-
-
-   	function My_First_Console_Log(value) {
-   		// Congratulations if you stepped in here! This class probably isn't for you.
-
-   		// But stick around, and tell your friends!
-
-
-		console.log('YAAAAY WELCOME TO THE CONSOLE! THIS IS WHERE WE CAN BEGIN THE ACTION!!!')
-		
-		console.log('But first the answer to your query!!!!! HERE IT IS:')
-
-   		console.log(value);
-		
-		console.log('YAAAAAAAY')
-
-		console.log('THATS ALL FOR NOW, FOLKS!')
-   	}
-
-
-
-
-  	function Welcome_To_The_World_Of_Tomorrow() {
-  		/* Welcome, welcome welcome! */
-  		debugger;
-  		/* 
-  			One and all, to the big show!
-
-  			Today you learn how to code.
-
-  			Click "Continue". It's the Play Button. Find it now.
-  		*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  		/* Nice, you made it to my secret spot! */
-  		debugger;
-  		/*
-
-  			There's not much here right now, but I got big plans I tell ya!
-
-  			I'm gonna put a couch over there, and a sunroof over here
-
-  			I got this sweet painting, click "Continue" to see it....
-
-  			If you want....
-  		*/
+/**
+ * IF YOU SEE THIS, YOU'RE PROBABLY VERY CONFUSED
+ * 
+ * Stay calm. Everything is okay.
+ *
+ * Breath. In. Out. Softly now. Softer.
+ *
+ * You are reading. It's okay. It's normal.
+ *
+ * Lots of people read.
+ *
+ * I read.
+ *
+ * You are now continuing to read. That is great! 
+ *
+ * More specificly, you are reading SOURCE CODE.
+ *
+ * Source code is any instruction that both 
+ * 
+ * a human (that's YOU!) and a computer (that's me)
+ *
+ * can read. That's right. You just learned something.
+ *
+ * Get used to it. Both the learning and the reading.
+ *
+ * A life without reading is a life without learning.
+ *
+ * Reading source code is important.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * 
+ *
+ *
+ * If none of that made sense to you, GREAT! 
+ *
+ * You are in the right place.
+ *
+ *
+ *
+ *
+ *
+ * This whole file has LOTS of new lines. Get used to it.
+ *
+ */
 
 
 
@@ -83,11 +59,256 @@ define(['d3', 'jquery', 'lodash', 'jquery.eventAggregator', 'knockout'],
 
 
 
-/* Scroll down to see the whole thing */
 
-// Then press "Continue"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function _________CHAPTER_ONE______THE_WELCOME________() {
+
 debugger;
-/*
+
+/**
+ * Welcome, welcome welcome!
+ *
+ * I don't mean to alarm you but...
+ * 
+ * YOU ARE FROZEN IN TIME!
+ *
+ * Continue reading to learn how to unfreeze your butt.
+ *
+ * ...
+ *  
+ *
+ * YOU have a great adventure ahead of you, human!
+ *
+ * Right now, you need to find the CONTINUE button.
+ *
+ * The CONTINUE button looks like a "Play" button.
+ *
+ * It kind of looks like this:  |>    but not really.
+ *
+ * From these words it should be SOMEWHERE TO THE RIGHT.
+ *
+ * Find the CONTINUE button now and click it once.
+ *
+ * That is your challenge, human adventurer.
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+debugger;
+/** 
+ * 
+ * NICE 
+ * 
+ * JOB
+ * 
+ * HUMAN!
+ *
+ * You just jumped that source code like a pro.
+ *
+ * See that blue line? That's your "Current Break"
+ * 
+ * Your "Current Break" should be at line 159 
+ * 
+ * right now. If not, the entire universe will
+ *
+ * explode in 3, 2, 1 - wait for it -
+ *
+ * or press CONTINUE again.
+ */
+debugger; // and again.
+debugger; // and again.
+debugger; // and again.
+debugger; // Hey I'm beginning to notice a pattern..
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+debugger;
+/**
+ * "debugger" is an INSTRUCTION
+ *
+ * to your computer. Your computer
+ *
+ * runs every instruction that your 
+ *
+ * "Current Break" moves over.
+ *
+ * The "debugger" instruction
+ *
+ * tells your computer to stop and wait
+ *
+ * for "user interaction." 
+ * 
+ * Guess what?
+ *
+ * YOU'RE THE USER *mind blown*
+ *
+ * Click CONTINUE.
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+debugger; 
+/**
+ *
+ * How do you feel about reading?
+ * 
+ * You are now continuing to read. That is great! 
+ *
+ * If you didn't know, you are reading SOURCE CODE.
+ *
+ * Source code is any instruction that both 
+ * 
+ * a human (that's YOU!) and a computer (that's me)
+ *
+ * can understand. I cannot "understand" or "feel" 
+ * 
+ * things like you humans (who'd want that anyways?),
+ *
+ * but I sure do understand instructions!
+ *
+ * Click CONTINUE
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+debugger;
+/**
+ * 
+ * The text between the slashes and asterisks 
+ * 
+ * are COMMENTS. Comments can contain anything,
+ *
+ * even beautiful posters:
 
 
 
@@ -132,10 +353,10 @@ debugger;
          `""'           `     ```"""""""""""""""         `""   ``
                                                            [banksy]dp 
 
-		Isn't that totally rad? Where should I put it? Next to the couch?
-      */                                                     
-      debugger;
-      // Click "Continue"
+Isn't that totally rad? Where should I put it? Next to the couch?
+*/                                                     
+debugger;
+// Click CONTINUE
 
 
 
@@ -152,32 +373,32 @@ debugger;
 
 
 
-      		// Okay let's get real
-      		debugger;
-      		// Press "Step Over"
-      		// It's the button that looks like a rainbow
-
-
-
-      		// Hey look, you're paused on a line other than
-      		// "debugger;" !!!!!!!!!!
-      		//
-      		// This is important. 
-      		//
-      		// How does that make you feel?
-      		// You will be able to tell me soon, young padawan.
-
-      		My_First_Console_Log(3*3*3*3*3*3*3*3*3*3*3*3*3*3*3)
-      		// Try pressing "Continue" now.
 
 
 
 
 
-			// You just ran some code! You did it! youyouyouyou
-			// But what's the answer?
-			// Open the "Console" to find out. It's a button that says "Console"
-      		debugger;
+debugger
+/**
+ * You have come to the end of Chapter One
+ *
+ * Welcome to the STEP DEBUGGER. The
+ *
+ * Step Debugger is the coolest tool 
+ *
+ * in the universe for cool humans (like YOU!)
+ *
+ * and cool computers (like me) to 
+ *
+ * communicate.
+ *
+ * You can scroll through this file to figure
+ *
+ * out what just happened to you, or you
+ *
+ * can just press CONTINUE one more time.
+ *
+ */
 
 
 
@@ -186,8 +407,70 @@ debugger;
 
 
 
-  	}
 
 
-  }
-);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+PRINT_NOW()
+
+
+
+// You're not supposed to see this. Just carry on.           
+// Play chapter one again, because until
+// Dev Tools is open, debugger's don't do anything
+setTimeout(_________CHAPTER_ONE______THE_WELCOME________, 1000);
+
+
+// Write a bunch of newlines...
+// ... to clear the Console
+// Change the blank string every time
+// to stop console from consolidating the lines 
+// into one
+for (var i = 0, a = ' '; i < 100; i++) console.log(a += ' ');
+
+}
+
+
+function My_First_Console_Log(value) {
+// Congratulations if you stepped in here! This class probably isn't for you.
+
+// But stick around...
+
+// ...and tell your friends!
+
+
+console.log('YAAAAY WELCOME TO THE CONSOLE! THIS IS WHERE WE CAN BEGIN THE ACTION!!!')
+
+console.log('But first the answer to your query!!!!! HERE IT IS:')
+
+console.log(value);
+
+console.log('YAAAAAAAY')
+
+console.log('THATS ALL FOR NOW, FOLKS!')
+}
+
+// Finally, after everything's prepped! Start the program.
+_________CHAPTER_ONE______THE_WELCOME________();
+
+
+
+
+
+// TESTING
+function PRINT_NOW() {
+    console.log(new Date().getTime())
+}
